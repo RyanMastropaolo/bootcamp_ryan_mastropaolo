99a25f8a8ff02ec18c442fd2a8ca3407366a85d5
Ryan Mastropaolo <ryanmastropaolo2022@gmail.com>
2026-08-18 08:33:40 -0400

    fixed typo

--- homework/homework02/src/config.py
+++ homework/homework02/src/config.py
@@ -5,7 +5,7 @@ from typing import Optional
 from dotenv import load_dotenv
 
 def load_env():
-    load_dotenv() # looks for a .env file in the current and parent dictionaries
+    load_dotenv() # looks for a .env file in the current and parent directories
 
 def get_key(name: str, default: Optional[str] = None) -> Optional[str]:
     return os.getenv(name, default)
